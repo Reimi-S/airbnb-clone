a3794408e5286f9233ec8d95463b3b2f7c468f57
REIMI SASAKI <rey.my013@gmail.com>
2021-11-09 18:49:15 -0800

    Created cards adn footer

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bd-gray-100 text-gray-500">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>Its a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Self coding</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>React</p>
+        <p>Tailwind</p>
+        <p>Next.js</p>
+        <p>Beautiful CSS</p>
+        <p>This is a great leaning </p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Safety information</p>
+        <p>Cancellation options</p>
+        <p>Our Covid-19 response</p>
+        <p>Report a neighbourhood</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
