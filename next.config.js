e0d4d032aff25c3883dad064aa977308c3aa1a9f
REIMI SASAKI <rey.my013@gmail.com>
2021-11-16 19:07:06 -0800

    created map section

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['links.papareact.com'],
   },
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoicmVpbWkiLCJhIjoiY2t3Mm5zajNpNHJwZDJvbW9idWR6eDRlYyJ9.o2BC--n1mnAU5TjD-WNLAQ',
+  },
 };
